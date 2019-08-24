cda625554adae296948d49b4c937b292e352a380
Tohll <Tohll@users.noreply.github.com>
2019-08-24 23:56:39 +0200

    Minor optimisations, hotfixes

--- src/anthill/controllers/Anthills.java
+++ src/anthill/controllers/Anthills.java
@@ -40,15 +40,8 @@ public class Anthills implements ObjectWithRange {
         this.sprite = new ImageIcon("img/Ant_hill.png");
         this.position = new Point(Configuration.SQUARE_SIDE / 2, Configuration.SQUARE_SIDE / 2);
         this.range = this._computeRange(this.position, this.width + 200, this.height + 200);
-        this.foodMonitor = new FoodMonitor(Configuration.STARTING_FOOD_STOCK, Configuration.MAX_SOLDIERS_COUNT + Configuration.MAX_PEONS_COUNT);
-    }
-
-    /**
-     * @param foodFound
-     * @throws InterruptedException
-     */
-    public void _foodAccessed() {
-        this.foodMonitor._interactionDone();
+        this.foodMonitor = new FoodMonitor(Configuration.STARTING_FOOD_STOCK,
+                Configuration.MAX_SOLDIERS_COUNT + Configuration.MAX_PEONS_COUNT);
     }
 
     /**
@@ -57,8 +50,16 @@ public class Anthills implements ObjectWithRange {
      * @return the int result of this.foodMonitor._getFood(quantity, ant)
      * @throws InterruptedException
      */
-    public int _accesFood(final int quantity, final AbsAnt ant, boolean isDeposing) throws InterruptedException {
+    public int _accesFood(final int quantity, final AbsAnt ant, final boolean isDeposing) throws InterruptedException {
         return this.foodMonitor._accesFood(quantity, ant, isDeposing);
+    }
+
+    /**
+     * @param foodFound
+     * @throws InterruptedException
+     */
+    public void _foodAccessed() {
+        this.foodMonitor._interactionDone();
     }
 
     public FoodMonitor _getFoodMonitor() {
