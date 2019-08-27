5c997f72cc8a9c2cefdf39bb7494c8fa91f75a7b
Tohll <Tohll@users.noreply.github.com>
2019-08-27 20:36:02 +0200

    soldiers slowly lose life when fighting and when not eating. They regenerate a bit when eating

--- src/anthill/utils/Configuration.java
+++ src/anthill/utils/Configuration.java
@@ -2,16 +2,48 @@ package anthill.utils;
 
 public class Configuration {
 
+    /**
+     * Number of food spots generated and hidden
+     */
     public static final int FOOD_SPOTS = 15;
-    public static final int HEIGHT = 1024;
+    /**
+     * Application height</br>
+     * Must be >= 500 && <= your resolution height
+     */
+    public static final int HEIGHT = 600;
+    /**
+     * Maximum peons that can be generated on the map
+     */
     public static final int MAX_PEONS_COUNT = 2500;
+    /**
+     * Maximum soldiers that can be generated on the map
+     */
     public static final int MAX_SOLDIERS_COUNT = 30;
+    /**
+     * Maximum predator life
+     */
     public static final int PREDATOR_LIFE = 70;
-    public static final int QUEEN_LIFE = 450;
+    /**
+     * Maximum queen life
+     */
+    public static final int QUEEN_LIFE = 500;
+    /**
+     * How many grass to be draw
+     */
     public static final int SCENERY_DENSITY = 5000;
-    public static final int SOLDIERS_LIFE = 10;
+    /**
+     * Maximum soldier life
+     */
+    public static final int SOLDIERS_LIFE = 50;
+    /**
+     * How much food the anthill start with
+     */
     public static final int STARTING_FOOD_STOCK = 4000;
-    public static final int WIDTH = 1280;
+    /**
+     * Application width</br>
+     * Must be >= 500 && <= your resolution width
+     */
+    public static final int WIDTH = 800;
 
     private Configuration() {
 
